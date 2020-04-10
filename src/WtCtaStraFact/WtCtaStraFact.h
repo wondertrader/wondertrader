0ffebcb2b069f08d1964708cfc503618cf7a4a77
WonderTrader <wondertrader@126.com>
2020-04-10 16:23:18 +0800

    添加了一个Cta策略工厂模块

---
+++ src/WtCtaStraFact/WtCtaStraFact.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "../WtCore/CtaStrategyDefs.h"
+
+USING_NS_OTP;
+
+class WtStraFact : public ICtaStrategyFact
+{
+public:
+	WtStraFact();
+	virtual ~WtStraFact();
+
+public:
+	virtual const char* getName() override;
+
+	virtual CtaStrategy* createStrategy(const char* name, const char* id) override;
+
+	virtual void enumStrategy(FuncEnumStrategyCallback cb) override;
+
+	virtual bool deleteStrategy(CtaStrategy* stra) override;	
+};
+
