3bfd1ff0227ec962f8773e6ef5bf571e9491f629
WonderTrader <wondertrader@126.com>
2020-04-13 17:03:14 +0800

    模块bug修正以后重新提交

--- py/wtpy/__init__.py
+++ py/wtpy/__init__.py
@@ -2,7 +2,8 @@ from .BaseDefs import BaseStrategy
 from .WtEngine import WtEngine
 from .Context import Context
 from .WtBtEngine import WtBtEngine
+from .WtDtEngine import WtDtEngine
 from .IndexWriter import MysqlIndexWriter
 from .DataReporter import DataReporter
 
-__all__ = ["BaseStrategy", "WtEngine", "Context", "WtBtEngine", "MysqlIndexWriter", "DataReporter"]
+__all__ = ["BaseStrategy", "WtEngine", "Context", "WtBtEngine", "MysqlIndexWriter", "DataReporter", "WtDtEngine"]
