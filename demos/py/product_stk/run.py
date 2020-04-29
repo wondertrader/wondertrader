208d933fa3cfc799c8450ac918880820b8c30b6f
WonderTrader <wondertrader@126.com>
2020-04-29 10:18:44 +0800

    [mod][demo] 实盘demo把数据组件demo也整合进去，便于用户直接复制使用

--- demos/py/product_stk/run.py
+++ demos/py/product_stk/run.py
@@ -4,7 +4,7 @@ from Strategies.DualThrust import StraDualThrust
 if __name__ == "__main__":
     #创建一个运行环境，并加入策略
     engine = WtEngine()
-    engine.init('.\\Common\\', "config.json")
+    engine.init('.\\Common\\', "config.json", "stk_comms.json", "stocks.json")
     
     straInfo = StraDualThrust(name='pydt_SH600000', code="SSE.600000", barCnt=50, period="d1", days=30, k1=0.1, k2=0.1, isForStk=True)
     engine.add_strategy(straInfo)
