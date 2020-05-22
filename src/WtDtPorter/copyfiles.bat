b99f4bdac38b3e1ff4ec42687f902c96ad4f5cff
WonderTrader <wondertrader@126.com>
2020-05-22 11:56:32 +0800

    [mod][cpp]复制依赖文件脚本修改

--- src/WtDtPorter/copyfiles.bat
+++ src/WtDtPorter/copyfiles.bat
@@ -1,9 +1,9 @@
 set env=%1
 set plat=%2
 
-xcopy ..\%plat%\%env%\WtDataWriter.dll ..\%plat%\%env%\QuoteFactory\ /C /Y
-xcopy ..\%plat%\%env%\ParserCTP.dll ..\%plat%\%env%\QuoteFactory\ /C /Y
-xcopy ..\%plat%\%env%\ParserXTP.dll ..\%plat%\%env%\QuoteFactory\ /C /Y
-xcopy ..\%plat%\%env%\ParserCTPMini.dll ..\%plat%\%env%\QuoteFactory\ /C /Y
-xcopy ..\%plat%\%env%\ParserFemas.dll ..\%plat%\%env%\QuoteFactory\ /C /Y
-xcopy ..\%plat%\%env%\ParseriTap.dll ..\%plat%\%env%\QuoteFactory\ /C /Y
+xcopy ..\%plat%\%env%\WtDataWriter.dll ..\%plat%\%env%\WtDtPorter\ /C /Y
+xcopy ..\%plat%\%env%\ParserCTP.dll ..\%plat%\%env%\WtDtPorter\ /C /Y
+xcopy ..\%plat%\%env%\ParserXTP.dll ..\%plat%\%env%\WtDtPorter\ /C /Y
+xcopy ..\%plat%\%env%\ParserCTPMini.dll ..\%plat%\%env%\WtDtPorter\ /C /Y
+xcopy ..\%plat%\%env%\ParserFemas.dll ..\%plat%\%env%\WtDtPorter\ /C /Y
+xcopy ..\%plat%\%env%\ParseriTap.dll ..\%plat%\%env%\WtDtPorter\ /C /Y
