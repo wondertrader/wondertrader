0a8c4d3769fc3b6719a8fd02045f1aa403f37471
wondertrader <wondertrader@126.com>
2021-06-08 13:00:28 +0800

    [add]增加一个LoaderRunner项目，用于调用合约加载模块

---
+++ src/LoaderRunner/LoaderRunner.cpp
@@ -0,0 +1,30 @@
+﻿// TestLoader.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+#include <string>
+
+#include "../Share/DLLHelper.hpp"
+
+typedef int (*LoaderRunner)();
+
+int main()
+{
+	DllHandle handle = DLLHelper::load_library("CTPLoader");
+	if (handle == NULL)
+	{
+		printf("module not found\r\n");
+	}
+	else
+	{
+		LoaderRunner runner = (LoaderRunner)DLLHelper::get_symbol(handle, "run");
+		if(runner == NULL)
+		{
+			printf("entrance not found\r\n");
+		}
+		else
+		{
+			return runner();
+		}
+	}
+
+	return 0;
+}
