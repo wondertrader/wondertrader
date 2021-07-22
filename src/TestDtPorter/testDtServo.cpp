f343876205f5599a1ef5c628a9a0e9ebc73c1760
wondertrader <wondertrader@126.com>
2021-07-22 16:42:39 +0800

    [mod]修改了测试WtDtServo的代码

--- src/TestDtPorter/testDtServo.cpp
+++ src/TestDtPorter/testDtServo.cpp
@@ -8,16 +8,22 @@ void on_bar(WTSBarStruct* curBar, bool isLast)
 	printf("%u\r\n", curBar->time);
 }
 
+void on_tick(WTSTickStruct* curTick, bool isLast)
+{
+	printf("%u.%u\r\n", curTick->action_date, curTick->action_time);
+}
+
 void testDtServo()
 {
-	initialize("dtservo.json", "logcfg.json");
+	initialize("dtservo.json", true);
 	printf(get_version());
 	printf("\r\n");
 
-	get_bars("CFFEX.IC.HOT", "m1", 202001010930, 202107211500, on_bar);
+	//get_bars("CFFEX.IC.HOT", "m1", 202001010930, 202107211500, on_bar);
 
-	get_bars("CFFEX.IC.HOT", "m3", 202001010930, 202107211500, on_bar);
+	get_bars_by_count("CFFEX.IC.HOT", "m3", 500, 202107221500, on_bar);
 
 	std::this_thread::sleep_for(std::chrono::seconds(125));
-	get_bars("CFFEX.IC.HOT", "m3", 202001010930, 202107211500, on_bar);
+	get_bars_by_count("CFFEX.IC.HOT", "m3", 500, 202107221500, on_bar);
+	//get_ticks_by_count("CFFEX.IC.HOT", 80000, 202107221000, on_tick);
 }
