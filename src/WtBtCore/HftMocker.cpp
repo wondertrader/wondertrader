a8912dbe2385fe6cb8278383fe86702eb3a002a4
wondertrader <wondertrader@126.com>
2022-11-15 17:44:02 +0800

    [mod]调整了一下HFTMocker的日志输出

--- src/WtBtCore/HftMocker.cpp
+++ src/WtBtCore/HftMocker.cpp
@@ -221,7 +221,7 @@ bool HftMocker::init_hft_factory(WTSVariant* cfg)
 	_error_rate = cfg->getUInt32("error_rate");
 	_match_this_tick = cfg->getBoolean("match_this_tick");
 
-	log_info("UFT match params: use_newpx-{}, error_rate-{}, match_this_tick-{}", _use_newpx, _error_rate, _match_this_tick);
+	log_info("HFT match params: use_newpx-{}, error_rate-{}, match_this_tick-{}", _use_newpx, _error_rate, _match_this_tick);
 
 	DllHandle hInst = DLLHelper::load_library(module);
 	if (hInst == NULL)
@@ -1088,7 +1088,7 @@ void HftMocker::do_set_position(const char* stdCode, double qty, double price /*
 	if (decimal::eq(pInfo._volume, qty))
 		return;
 
-	log_info("[{:04d}.{:05d}] {} position updated: {} -> {}", _replayer->get_min_time(), _replayer->get_secs(), stdCode, pInfo._volume, qty);
+	log_debug("[{:04d}.{:05d}] {} position updated: {} -> {}", _replayer->get_min_time(), _replayer->get_secs(), stdCode, pInfo._volume, qty);
 
 	WTSCommodityInfo* commInfo = _replayer->get_commodity_info(stdCode);
 	if (commInfo == NULL)
