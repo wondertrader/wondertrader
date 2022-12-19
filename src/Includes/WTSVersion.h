f25c579d2f877743e024e326b282ae13b555837f
wondertrader <wondertrader@126.com>
2022-12-19 09:47:52 +0800

    [mod]将版本号更新为0.9.6

--- src/Includes/WTSVersion.h
+++ src/Includes/WTSVersion.h
@@ -2,6 +2,6 @@
 
 #define WT_VER_MAJOR 0
 #define WT_VER_MINOR 9
-#define WT_VER_PATCH 4
+#define WT_VER_PATCH 6
 
-static const char WT_VERSION[] = "v0.9.5";
+static const char WT_VERSION[] = "v0.9.6";
