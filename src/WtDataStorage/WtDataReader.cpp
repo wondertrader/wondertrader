99725b4065d6366fcba485b33d7143a7426a091b
wondertrader <wondertrader@126.com>
2023-03-15 22:47:52 +0800

    [fix]修正了每天开盘前，读取不到K线数据的bug

--- src/WtDataStorage/WtDataReader.cpp
+++ src/WtDataStorage/WtDataReader.cpp
@@ -1752,6 +1752,14 @@ WTSKlineSlice* WtDataReader::readKlineSlice(const char* stdCode, WTSKlinePeriod 
 				}
 			}
 		}
+		else
+		{
+			rtCnt = 0;
+			hisCnt = count;
+			hisCnt = min(hisCnt, (uint32_t)barsList._bars.size());
+			head = &barsList._bars[barsList._bars.size() - hisCnt];
+			slice = WTSKlineSlice::create(stdCode, period, 1, head, hisCnt);
+		}
 	}
 	else
 	{
