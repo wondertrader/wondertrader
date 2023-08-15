927c93fec7963fe80978dce969609bc458937014
wondertrader <wondertrader@126.com>
2023-08-16 00:33:13 +0800

    [mod]完善了linux的复制脚本

--- copy_bins_linux.sh
+++ copy_bins_linux.sh
@@ -1,8 +1,20 @@
 despath=$1
 if test "$despath"==""; then 
 	despath="../wtpy"
-	echo $despath
 fi
-cp -rf ./src/build_all/build_x64/Release/bin/Loader/*.so $despath/wtpy/wrapper/linux
-cp -rf ./src/build_all/build_x64/Release/bin/WtBtPorter/*.so $despath/wtpy/wrapper/linux
-cp -rf ./src/build_all/build_x64/Release/bin/WtDtPorter/*.so $despath/wtpy/wrapper/linux
+
+echo "wtpy path is $despath"
+
+root="./src/build_all/build_x64/Release/bin"
+folders=("Loader" "WtBtPorter" "WtDtPorter" "WtPorter")
+for folder in ${folders[@]}
+do
+	cp -rf $root/$folder/*.so $despath/wtpy/wrapper/linux
+	for file in `ls $root/$folder`
+	do
+		if [ -d $root"/"$folder"/"$file ]
+		then
+			cp -rf $root/$folder/$file/*.so $despath/wtpy/wrapper/linux/$file
+		fi
+	done
+done
