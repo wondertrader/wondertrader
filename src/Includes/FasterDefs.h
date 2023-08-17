4d15a3e877196c6dd21a5114fd30e51472ab0276
wondertrader <wondertrader@126.com>
2023-08-17 17:35:37 +0800

    [mod]增加了一个注释，主要对hash容器的性能对比加了一个外部参考链接

--- src/Includes/FasterDefs.h
+++ src/Includes/FasterDefs.h
@@ -20,6 +20,8 @@
   *	ankerl写入速度比robin好很多，大概快1/3，尤其数据量在40w以内的时候
   *	但是robin的读取速度比robin好，不过到了30w条数据以内，差别就不大
   *	按照wondertrader的场景，还是ankerl要好很多
+  * 具体可以参考以下页面的性能对比
+  * https://martin.ankerl.com/2022/08/27/hashmap-bench-01/#benchmark-results-table
   */
 
 NS_WTP_BEGIN
